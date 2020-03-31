decb1c9596f8037a3d7bb261b40f5f9f43985e12
adam allalou <aabou9@gmail.com>
2020-03-31 13:48:14 +0100

    add enumerables.rb

---
+++ enumerables.rb
@@ -0,0 +1,26 @@
+module Enumerable
+  def my_each
+    # your code here
+  end
+
+  def my_select
+      # your code here
+  end
+
+  def my_all?
+  # your code here
+  end
+
+  def my_any?
+  # your code here
+  end
+
+  def my_none?
+  # your code here
+  end
+
+  def my_count
+  # your code here
+  end
+end
+
