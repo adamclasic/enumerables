ee7863b91578e577acecdd47bcc1e81a153ca5f5
adam allalou <aabou9@gmail.com>
2020-04-07 13:59:34 +0100

    add my inject

--- enumerables.rb
+++ enumerables.rb
@@ -5,8 +5,8 @@ module Enumerable
   def my_each
     return to_enum(:my_each) unless block_given?
       i = 0
-      while i < self.length do
-        yield(self[i])
+      while i < self.size do
+        yield(self.to_a[i])
         i += 1
       end
   end
@@ -113,18 +113,7 @@ module Enumerable
     return counter
   end
 
-
-  def my_count1(param = nil)
-    counter = 0
-    return length if param.nil? && !block_given?
-    my_each do |i|
-      condition = yield(i) if block_given?
-      counter += 1 if condition || i == param
-    end
-    counter
-  end
-
-    # my_map
+  # my_map
 
   def my_map(prc=nil)
     return to_enum(:my_map) unless block_given? || !prc.nil?
@@ -139,70 +128,32 @@ module Enumerable
     end
   end
 
+  # my_inject
+
+  def my_inject(init = nil, sym = nil)
+    if block_given?
+      if !init.nil?
+        num = init
+        my_each { |elem| num = yield(num, elem) }
+      else
+        num = first
+        to_a[1..-1].my_each { |elem| num = yield(num, elem) }
+      end
+    elsif !sym.nil?
+      num = init
+      my_each { |elem| num = num.send(sym, elem) }
+    elsif !init.nil?
+      num = first
+      to_a[1..-1].my_each { |elem| num = num.send(init, elem) }
+    end
+    num
+  end
 end
 
-puts "---------"
-puts "my_each"
-puts "--------"
+# multiply_els
 
-puts [1, 2, 3].my_each { |elem| print "#{elem + 1} "} # => 2 3 4
+def multiply_els(arr)
+  return arr.my_inject(:*)
+end
 
-puts "---------"
-puts "my_each_with_index?"
-puts "--------"
-
-puts [5, 7, 0, 1, 2, 3].my_each_with_index { |elem, idx| puts "#{elem} : #{idx}" }
-
-puts "---------"
-puts "my_all?"
-puts "--------"
-
-p [-8, -9, -6].my_all? { |n| n < 0 } # => true
-p [3, 5, 8, 11].my_all? { |n| n.odd? } # => false
-p [-8, -9, -6, 0].my_all? { |n| n < 0 } # => false
-p [-8, -9, -6, 0].my_all? # => true
-p [-8, -9, -6, nil].my_all? # => false
-p [-8, -9, -6, false].my_all? # => false
-p [].my_all? # => true
-
-puts "---------"
-puts "my_any?"
-puts "--------"
-
-p [-8, -9, -6].my_any? { |n| n < 0 } # => true
-p [3, 5, 8, 11].my_any? { |n| n.odd? } # => true
-p [-8, -9, -6, 0].my_any? { |n| n < 0 } # => true
-p [-8, -9, -6, 0].my_any? # => false
-p [-8, -9, -6, nil].my_any? # => true
-p [-8, -9, -6, false].my_any? # => ture
-p [].my_any? # => false
-
-puts "---------"
-puts "my_none?"
-puts "--------"
-p [-8, -9, -6].my_none? { |n| n < 0 } # => false
-p [3, 5, 8, 11].my_none? { |n| n.odd? } # => false
-p [-8, -9, -6, 0].my_none? { |n| n < 0 } # => false
-p [-8, -9, -6, 0].my_none? # => true
-p [-8, -9, -6, nil].my_none? # => false
-p [-8, -9, -6, false].my_none? # => false
-p [].my_none? # => true
-
-puts "---------"
-puts "my_count"
-puts "--------"
-p [1,4,3,8].my_count { |n| n.even? } # => 2
-p ["DANIEL", "JIA", "KRITI", "dave"].my_count { |s| s == s.upcase } # => 3
-p ["daniel", "jia", "kriti", "dave"].my_count { |s| s == s.upcase } # => 0
-p [1,2,3].my_count # => 3
-p [1,1,1,2,3].my_count(1) # => 3
-
-puts "------"
-puts "my_map"
-puts "------"
-
-p [1,2,3].my_map { |n| 2 * n } # => [2,4,6]
-p ["Hey", "Jude"].my_map { |word| word + "?" } # => ["Hey?", "Jude?"]
-p [false, true].my_map { |bool| !bool } # => [true, false]
-  my_proc = Proc.new {|num| num > 10 }
-p [18, 22, 5, 6].my_map(my_proc) {|num| num < 10 } # => true true false false
+p multiply_els([2,4,5]) #=> 40
